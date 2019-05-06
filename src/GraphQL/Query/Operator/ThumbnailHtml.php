8c9c101a2c52f172ab5b9f75f7eacb940d203337
Niklas <niklas.brunberg@ateles.se>
2019-05-06 11:20:57 +0200

    Head into fork (#2)
    * Adds ability to get system column for Published status
    
    * Includes @throws in ConfigController method docs
    
    * Removes debug code from QueryType
    
    * Adds 'published' as a queryable field argument
    
    * Adds the ability to filter on published status on object lists
    
    * Includes missing comma
    
    * Initial commit
    
    * Bug fix for data objects and assets (info logging error)
    
    * Thumbnail HTML: Working concept
    
    * Thumbnail HTML: Adds missing admin translation

---
+++ src/GraphQL/Query/Operator/ThumbnailHtml.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @category   Pimcore
+ * @package    Object
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace Pimcore\Bundle\DataHubBundle\GraphQL\Query\Operator;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use Pimcore\Bundle\DataHubBundle\GraphQL\Service;
+use Pimcore\Model\Asset;
+
+/**
+ * Class ThumbnailHtml
+ * @package Pimcore\Bundle\DataHubBundle\GraphQL\Query\Operator
+ */
+class ThumbnailHtml extends AbstractOperator
+{
+    private $thumbnailHtmlConfig;
+
+    /**
+     * ThumbnailHtml constructor.
+     *
+     * @param array $config
+     * @param null $context
+     */
+    public function __construct(array $config = [], $context = null)
+    {
+        parent::__construct($config, $context);
+
+        $this->thumbnailHtmlConfig = $config['thumbnailHtmlConfig'];
+    }
+
+    /**
+     * @param \Pimcore\Model\Element\ElementInterface $element
+     * @param ResolveInfo|null $resolveInfo
+     *
+     * @return \stdClass|null
+     */
+    public function getLabeledValue($element, ResolveInfo $resolveInfo = null)
+    {
+        $result = new \stdClass();
+        $result->label = $this->label;
+        if (!$this->thumbnailHtmlConfig) {
+            return $result;
+        }
+
+        $children = $this->getChilds();
+
+        if (!$children) {
+            return $result;
+        } else {
+            $c = $children[0];
+
+            $service = \Pimcore::getContainer()->get(Service::class);
+            $valueResolver = $service->buildValueResolverFromAttributes($c);
+
+            $childResult = $valueResolver->getLabeledValue($element, $resolveInfo);
+            if ($childResult) {
+                $result->value = null;
+                if ($childResult->value instanceof Asset\Image || $childResult->value instanceof Asset\Video) {
+                    $childValue = $result->value = $childResult->value;
+                    $thumbnail = $childValue->getThumbnail($this->thumbnailHtmlConfig, false);
+                    $result->value = $thumbnail->getHtml();
+                }
+            }
+        }
+
+        return $result;
+    }
+}
