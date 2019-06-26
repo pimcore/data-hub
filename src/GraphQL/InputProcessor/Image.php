35450e51faab0fdaccf3777a244f54af273ac0a9
Divesh <divesh.pahuja@pimcore.com>
2019-06-26 13:05:21 +0200

    Add Support for Image mutation - Image type restriction

--- src/GraphQL/InputProcessor/Image.php
+++ src/GraphQL/InputProcessor/Image.php
@@ -17,6 +17,7 @@ namespace Pimcore\Bundle\DataHubBundle\GraphQL\InputProcessor;
 
 use GraphQL\Type\Definition\ResolveInfo;
 use Pimcore\Bundle\DataHubBundle\GraphQL\Service;
+use Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle;
 use Pimcore\Model\DataObject\Concrete;
 use Pimcore\Model\Asset;
 
@@ -34,14 +35,24 @@ class Image extends Base
     public function process(Concrete $object, $newValue, $args, $context, ResolveInfo $info)
     {
         $attribute = $this->getAttribute();
-        Service::setValue($object, $attribute, function($container, $setter) use ($newValue) {
 
-            $asset = null;
+        if(!array_key_exists('id',$newValue) && PimcoreDataHubBundle::getNotAllowedPolicy() == PimcoreDataHubBundle::NOT_ALLOWED_POLICY_EXCEPTION) {
+            throw new \Exception("Field {$attribute}.id was not provided.");
+        }
+
+        Service::setValue($object, $attribute, function($container, $setter) use ($newValue, $attribute) {
+            $image = null;
+
             if (is_array($newValue)) {
                 $asset = Asset::getById($newValue["id"]);
+                if ($asset instanceof Asset\Image) {
+                    $image = $asset;
+                } else if (PimcoreDataHubBundle::getNotAllowedPolicy() == PimcoreDataHubBundle::NOT_ALLOWED_POLICY_EXCEPTION) {
+                    throw new \Exception("Invalid Asset(Image) ID provided for field " . $attribute);
+                }
             }
 
-            return $container->$setter($asset);
+            return $container->$setter($image);
         });
     }
 }
