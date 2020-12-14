e1f7ba6934c6232392d079ceaa49c644d6158d9f
Fabian Jucker <fabianju@gmx.ch>
2020-12-14 14:57:26 +0100

    read query variables after preExecute event

--- src/Controller/WebserviceController.php
+++ src/Controller/WebserviceController.php
@@ -145,6 +145,10 @@ class WebserviceController extends FrontendController
 
             $this->eventDispatcher->dispatch($event, ExecutorEvents::PRE_EXECUTE);
 
+            if ($event->getRequest() instanceof Request) {
+                $variableValues =  $event->getRequest()->get('variables');
+            }
+
             $result = GraphQL::executeQuery(
                 $event->getSchema(),
                 $event->getQuery(),
