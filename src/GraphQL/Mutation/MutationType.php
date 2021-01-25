ffca25d17304209b9a794a9b1e20a484715ded17
Nelson Garcia <nagarciah@yahoo.com>
2021-01-25 16:55:01 +0100

    Fixed 'Call to a member function resolveObjectGetter() on array' when creating an image via GraphQL

--- src/GraphQL/Mutation/MutationType.php
+++ src/GraphQL/Mutation/MutationType.php
@@ -831,7 +831,7 @@ class MutationType extends ObjectType
                         'type' => $assetType,
                         'resolve' => static function ($value, $args, $context, ResolveInfo $info) use ($queryResolver) {
                             $args["id"] = $value["id"];
-                            $value = $queryResolver->resolveObjectGetter($value, $args, $context, $info);
+                            $value = $queryResolver($value, $args, $context, $info);
 
                             return $value;
                         }
