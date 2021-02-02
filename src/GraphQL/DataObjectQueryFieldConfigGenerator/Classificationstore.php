1a1c45714245c9f7d7c03a3ea2e81187d780f862
Josef Aichhorn <josef.aichhorn@elements.at>
2021-02-02 09:05:01 +0100

    [ClassificationStore] respect inheritance of active groups

--- src/GraphQL/DataObjectQueryFieldConfigGenerator/Classificationstore.php
+++ src/GraphQL/DataObjectQueryFieldConfigGenerator/Classificationstore.php
@@ -43,7 +43,12 @@ class Classificationstore extends Base
                     $language = isset($args["language"]) ? $args["language"] : null;
                     /** @var  $csField \Pimcore\Model\DataObject\Classificationstore*/
                     $csField = $value[$fieldName];
-                    $activeGroups = $csField->getActiveGroups();
+
+                    $fd = new Data\Classificationstore();
+                    $fd->setName($fieldName);
+                    $activeGroups = [];
+                    $activeGroups = $fd->recursiveGetActiveGroupsIds($csField->getObject(), $activeGroups);
+
                     $result = [];
                     foreach ($activeGroups as $groupId => $enabled) {
                         // in case group name and description is not needed this can be optimized
