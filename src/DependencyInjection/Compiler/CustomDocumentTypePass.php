f3416e012a576343f6456e1305a129ce909033db
dvesh3 <divesh.pahuja@pimcore.com>
2021-10-01 17:52:21 +0200

    [Documents] Add support for custom document types with tagged services: pimcore.datahub.graphql.documenttype.customtype

---
+++ src/DependencyInjection/Compiler/CustomDocumentTypePass.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+declare(strict_types=1);
+
+namespace Pimcore\Bundle\DataHubBundle\DependencyInjection\Compiler;
+
+use Pimcore\Bundle\DataHubBundle\GraphQL\DocumentType\DocumentType;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class CustomDocumentTypePass implements CompilerPassInterface
+{
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $documentTypeService = $container->getDefinition(DocumentType::class);
+
+        $resolvers = $container->findTaggedServiceIds('pimcore.datahub.graphql.documenttype.customtype');
+
+        $dataTypes = [];
+
+        foreach ($resolvers as $id => $tagEntries) {
+            foreach ($tagEntries as $tagEntry) {
+                $typeDef = $container->getDefinition($id);
+                $dataTypes[$tagEntry['id']] = $typeDef;
+            }
+        }
+
+        $documentTypeService->addMethodCall('registerCustomDataType', [$dataTypes]);
+    }
+}
