22b56b9cc2352a0b93870d3720e14a93c3a1e8c0
JiaJia Ji <kingjia90@gmail.com>
2022-04-04 17:24:21 +0200

    [env setup] `app` folder change to 6.9 to x

--- .github/ci/scripts/setup-pimcore-x-environment.sh
+++ .github/ci/scripts/setup-pimcore-x-environment.sh
@@ -10,7 +10,7 @@ cp -r .github/ci/pimcore_x/files/templates templates
 cp -r .github/ci/pimcore_x/files/bin/console bin/console
 chmod 755 bin/console
 cp -r .github/ci/pimcore_x/files/kernel kernel
-cp -r .github/ci/pimcore_6_9/files/app app
+cp -r .github/ci/pimcore_x/files/app app
 cp -r .github/ci/pimcore_x/files/public public
 cp .github/ci/pimcore_x/files/extensions.template.php var/config/extensions.php
 cp .github/ci/pimcore_x/files/.env ./
