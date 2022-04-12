c15a2be833a11d6e75921fd486bc04fe6195408d
mcop1 <89011527+mcop1@users.noreply.github.com>
2022-04-12 09:49:49 +0200

    [Feature]: GraphQL Mutation for Advanced Many-to-Many Object relation (#507)
    * [Feature]: GraphQL Mutation for Advanced Many-to-Many Object relation
    
    * Added to doc
    
    * added advanced many to many object relations
    
    * Apply php-cs-fixer changes
    
    * phpstan
    
    Co-authored-by: mcop1 <mcop1@users.noreply.github.com>

---
+++ src/GraphQL/DataObjectMutationFieldConfigGenerator/AdvancedManyToManyRelation.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\DataHubBundle\GraphQL\DataObjectMutationFieldConfigGenerator;
+
+use GraphQL\Type\Definition\Type;
+use Pimcore\Bundle\DataHubBundle\GraphQL\DataObjectType\ElementDescriptorInputType;
+use Pimcore\Bundle\DataHubBundle\GraphQL\Service;
+
+class AdvancedManyToManyRelation extends Base
+{
+    protected $elementInputType;
+    protected $fieldDefinition;
+
+    /**
+     * @param Service $graphQlService
+     * @param ElementDescriptorInputType $elementInputType
+     */
+    public function __construct(Service $graphQlService, ElementDescriptorInputType $elementInputType)
+    {
+        $this->elementInputType = $elementInputType;
+        parent::__construct($graphQlService);
+    }
+
+    /** {@inheritdoc } */
+    public function getGraphQlMutationFieldConfig($nodeDef, $class, $container = null, $params = [])
+    {
+        $processor = new \Pimcore\Bundle\DataHubBundle\GraphQL\DataObjectInputProcessor\AdvancedManyToManyRelation($nodeDef);
+        $processor->setGraphQLService($this->getGraphQlService());
+
+        $inputType = $this->getGraphQlService()->getDataObjectTypeDefinition('elementdescriptor_input');
+
+        return [
+            'arg' => ['type' => Type::listOf($inputType)],
+            'processor' => [$processor, 'process']
+        ];
+    }
+}
--- src/GraphQL/DataObjectType/ElementDescriptorInputType.php
+++ src/GraphQL/DataObjectType/ElementDescriptorInputType.php
@@ -17,20 +17,39 @@ namespace Pimcore\Bundle\DataHubBundle\GraphQL\DataObjectType;
 
 use GraphQL\Type\Definition\InputObjectType;
 use GraphQL\Type\Definition\Type;
+use Pimcore\Bundle\DataHubBundle\GraphQL\Resolver\ObjectMetadata;
 use Pimcore\Bundle\DataHubBundle\GraphQL\Service;
 use Pimcore\Bundle\DataHubBundle\GraphQL\Traits\ServiceTrait;
+use Pimcore\Model\DataObject\ClassDefinition\Data;
 
 class ElementDescriptorInputType extends InputObjectType
 {
     use ServiceTrait;
 
     /**
+     * @var null
+     */
+    protected $class;
+
+    /** @var Data */
+    protected $fieldDefinition;
+
+    /**
      * @param Service $graphQlService
+     * @param Data|null $fieldDefinition
+     * @param null $class
      * @param array $config
      * @param array $context
      */
-    public function __construct(Service $graphQlService, $config = ['name' => 'ElementDescriptorInput'], $context = [])
+    public function __construct(Service $graphQlService,
+                                Data $fieldDefinition = null,
+                                $class = null,
+                                $config = ['name' => 'ElementDescriptorInput'],
+                                $context = [])
     {
+        $this->class = $class;
+        $this->fieldDefinition = $fieldDefinition;
+
         $this->setGraphQLService($graphQlService);
         $this->build($config);
         parent::__construct($config);
@@ -41,11 +60,17 @@ class ElementDescriptorInputType extends InputObjectType
      */
     public function build(&$config)
     {
+        $resolver = new ObjectMetadata($this->fieldDefinition, $this->class, $this->getGraphQlService()->getObjectFieldHelper());
+
         $config['fields'] = [
             'type' => Type::string(),
             'id' => Type::int(),
-            'fullpath' => Type::string()
+            'fullpath' => Type::string(),
+            'metadata' => [
+                'type' => Type::listOf(new ElementMetadataKeyValuePairInputType()),
+                'resolve' => [$resolver, 'resolveMetadata']
+            ]
         ];
-        $config['description'] = 'type can be omitted for mutations only allowing one type, e.g. many-to-many-objects';
+        $config['description'] = 'type can be omitted for mutations only allowing one type, e.g. many-to-many-objects.';
     }
 }
