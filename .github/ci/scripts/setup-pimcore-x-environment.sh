1e191ec942583ef39da7e0283ab549e8fb587bf4
mcop1 <marco.perberschlager@pimcore.com>
2022-05-03 14:34:51 +0200

    PHPStan + Codeception Tests

--- .github/ci/scripts/setup-pimcore-x-environment.sh
+++ .github/ci/scripts/setup-pimcore-x-environment.sh
@@ -5,11 +5,11 @@ set -eu
 mkdir -p var/config
 mkdir -p bin
 
+cp .github/ci/pimcore_x/files/.env .
 cp -r .github/ci/pimcore_x/files/config/. config
-cp -r .github/ci/pimcore_x/files/templates templates
+cp -r .github/ci/pimcore_x/files/templates/. templates
 cp -r .github/ci/pimcore_x/files/bin/console bin/console
 chmod 755 bin/console
-cp -r .github/ci/pimcore_x/files/kernel kernel
-cp -r .github/ci/pimcore_x/files/public public
+cp -r .github/ci/pimcore_x/files/kernel/. kernel
+cp -r .github/ci/pimcore_x/files/public/. public
 cp .github/ci/pimcore_x/files/extensions.template.php var/config/extensions.php
-cp .github/ci/pimcore_x/files/.env ./
