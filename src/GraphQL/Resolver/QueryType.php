dd96d06c1b2b8b613de36e33b68a150e14c97112
robertSt7 <robertSt7@users.noreply.github.com>
2022-07-22 12:03:34 +0000

    Apply php-cs-fixer changes

--- src/GraphQL/Resolver/QueryType.php
+++ src/GraphQL/Resolver/QueryType.php
@@ -233,7 +233,7 @@ class QueryType
     public function resolveTranslationGetter(mixed $value = null, array $args = [], array $context = [], ResolveInfo $resolveInfo = null): array
     {
         if (empty($args['key'])) {
-            throw new \Exception("Argument key is mandatory");
+            throw new \Exception('Argument key is mandatory');
         }
 
         $domain = 'messages';
