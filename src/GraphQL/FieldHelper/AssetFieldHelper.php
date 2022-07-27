80f5a07ca4e7aa4ffd739ef75d77a9afc39c91fd
mcop1 <marco.perberschlager@pimcore.com>
2022-07-27 12:53:30 +0200

    Reverted phpstan-baseline.neon, added check instead

--- src/GraphQL/FieldHelper/AssetFieldHelper.php
+++ src/GraphQL/FieldHelper/AssetFieldHelper.php
@@ -43,12 +43,16 @@ class AssetFieldHelper extends AbstractFieldHelper
 
     public function getImageDocumentThumbnail(Asset $asset, string | Image\Thumbnail\Config $thumbNailConfig, string $thumbNailFormat = null): mixed
     {
+        $thumb = null;
         if ($asset instanceof Asset\Document || $asset instanceof Asset\Video) {
             $thumb = $asset->getImageThumbnail($thumbNailConfig);
         } elseif ($asset instanceof Asset\Image) {
             $thumb = $asset->getThumbnail($thumbNailConfig, false);
         }
-        if (isset($thumbNailFormat) && method_exists($thumb, 'getAsFormat') && !($asset instanceof Asset\Video)) {
+        if (isset($thumb) &&
+            isset($thumbNailFormat) &&
+            method_exists($thumb, 'getAsFormat') &&
+            !($asset instanceof Asset\Video)) {
             $thumb = $thumb->getAsFormat($thumbNailFormat);
         }
 
