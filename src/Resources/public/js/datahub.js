7553edc6da8b486a14f81e4918da9d69320491ca
robertSt7 <robert.steinkellner@pimcore.com>
2022-07-28 13:51:15 +0200

    Improve: Replace plugin with event listener

--- src/Resources/public/js/datahub.js
+++ src/Resources/public/js/datahub.js
@@ -11,62 +11,49 @@
  * @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
-pimcore.registerNS("pimcore.plugin.datahub");
+document.addEventListener(pimcore.events.pimcoreReady, (e) => {
+    const perspectiveCfg = pimcore.globalmanager.get("perspective");
 
-pimcore.plugin.datahub = Class.create(pimcore.plugin.admin, {
-    getClassName: function () {
-        return "pimcore.plugin.datahub";
-    },
+    if (perspectiveCfg.inToolbar("datahub") === false) {
+        return
+    }
 
-    initialize: function () {
-        pimcore.plugin.broker.registerPlugin(this);
-    },
+    const user = pimcore.globalmanager.get("user");
+    if (user.admin || user.isAllowed("plugin_datahub_config")) {
 
-    pimcoreReady: function (params, broker) {
-        var perspectiveCfg = pimcore.globalmanager.get("perspective");
+        let navEl = Ext.get('pimcore_menu_search').insertSibling('<li id="pimcore_menu_datahub" data-menu-tooltip="'
+            + t('plugin_pimcore_datahub_toolbar') +
+            '" class="pimcore_menu_item pimcore_menu_needs_children"><img alt="datahub" src="/bundles/pimcoreadmin/img/flat-white-icons/mind_map.svg"></li>', 'before');
 
-        if (perspectiveCfg.inToolbar("datahub") == false) {
-            return
-        }
-
-        var user = pimcore.globalmanager.get("user");
-        if (user.admin || user.isAllowed("plugin_datahub_config")) {
-
-            let navEl = Ext.get('pimcore_menu_search').insertSibling('<li id="pimcore_menu_datahub" data-menu-tooltip="'
-                + t('plugin_pimcore_datahub_toolbar') +
-                '" class="pimcore_menu_item pimcore_menu_needs_children"><img src="/bundles/pimcoreadmin/img/flat-white-icons/mind_map.svg"></li>', 'before');
-
-            navEl.on('mousedown', function () {
-                try {
-                    pimcore.globalmanager.get("plugin_pimcore_datahub_config").activate();
-                }
-                catch (e) {
-                    pimcore.globalmanager.add("plugin_pimcore_datahub_config", new pimcore.plugin.datahub.config());
-                }
-            });
-
-            pimcore.helpers.initMenuTooltips();
-        }
-    },
-
-    onPerspectiveEditorLoadStructureForPermissions: function (context, structure) {
-        if(context == 'toolbar') {
-            structure['datahub'] = {};
-        }
-    },
-
-    onPerspectiveEditorLoadPermissions: function (context, menu, permissions) {
-        if(context == 'toolbar' && menu == 'datahub') {
-            if(permissions[context][menu] == undefined) {
-                permissions[context][menu] = [];
+        navEl.on('mousedown', function () {
+            try {
+                pimcore.globalmanager.get("plugin_pimcore_datahub_config").activate();
+            } catch (e) {
+                pimcore.globalmanager.add("plugin_pimcore_datahub_config", new pimcore.plugin.datahub.config());
             }
-            if(permissions[context][menu].indexOf('hidden') == -1) {
-                permissions[context][menu].push('hidden');
-            }
-        }
+        });
+
+        pimcore.helpers.initMenuTooltips();
     }
 });
 
+document.addEventListener("pimcore.perspectiveEditor.permissions.structure.load", (e) => {
+    if (e.detail.context === 'toolbar') {
+        e.detail.structure['datahub'] = {};
+    }
+});
 
-var dtaPlugin = new pimcore.plugin.datahub();
+document.addEventListener("pimcore.perspectiveEditor.permissions.load", (e) => {
+    const context = e.detail.context;
+    const menu = e.detail.menu;
+    const permissions = e.detail.permissions;
 
+    if (context === 'toolbar' && menu === 'datahub') {
+        if (permissions[context][menu] === undefined) {
+            permissions[context][menu] = [];
+        }
+        if (permissions[context][menu].indexOf('hidden') === -1) {
+            permissions[context][menu].push('hidden');
+        }
+    }
+});
