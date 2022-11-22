13f7baeb5d9cbfb79fda3f76c370492430b1dab4
mcop1 <mcop1@users.noreply.github.com>
2022-11-22 11:15:35 +0000

    Apply php-cs-fixer changes

--- src/GraphQL/ClassificationstoreFeatureQueryTypeGenerator/QuantityValue.php
+++ src/GraphQL/ClassificationstoreFeatureQueryTypeGenerator/QuantityValue.php
@@ -15,7 +15,6 @@
 
 namespace Pimcore\Bundle\DataHubBundle\GraphQL\ClassificationstoreFeatureQueryTypeGenerator;
 
-use GraphQL\Type\Definition\Type;
 use Pimcore\Bundle\DataHubBundle\GraphQL\ClassificationstoreFeatureType\QuantityValueType;
 
 class QuantityValue extends Base
