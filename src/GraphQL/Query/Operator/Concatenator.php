cec9b00edcafcbfd74b2b5cab3d05f99c4bc15f4
lukmzig <30526586+lukmzig@users.noreply.github.com>
2023-01-23 15:30:49 +0100

    [Bug]: Some Operators don't work (#666)
    * save some local changes after latest bugs
    
    * fix: operators processing unexpected data
    
    * Apply php-cs-fixer changes
    
    * fix: ignore false failing test
    
    * fix: try to get concrete DAO
    
    * Apply php-cs-fixer changes
    
    Co-authored-by: lukmzig <lukmzig@users.noreply.github.com>

--- src/GraphQL/Query/Operator/Concatenator.php
+++ src/GraphQL/Query/Operator/Concatenator.php
@@ -58,11 +58,10 @@ class Concatenator extends AbstractOperator
 
         foreach ($children as $c) {
             $valueResolver = $this->getGraphQlService()->buildValueResolverFromAttributes($c);
-
-            $childResult = $valueResolver->getLabeledValue($element, $resolveInfo);
-            if ($childResult == null) {
+            if (!$childResult = $valueResolver->getLabeledValue($element, $resolveInfo)) {
                 continue;
             }
+
             $childValues = $childResult->value;
             if ($childValues && !is_array($childValues)) {
                 $childValues = [$childValues];
