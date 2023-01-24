048be2f3020196d1ab8c8254f5c14d44adceacab
mattamon <matthias.schuhmayer@pimcore.com>
2023-01-24 11:54:43 +0100

    Creating class instead of just listeners for datahub.js Adding pimcore 11 new menu creation support

--- src/Resources/public/js/datahub.js
+++ src/Resources/public/js/datahub.js
@@ -11,49 +11,92 @@
  * @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
-document.addEventListener(pimcore.events.pimcoreReady, (e) => {
-    const perspectiveCfg = pimcore.globalmanager.get("perspective");
+pimcore.registerNS("pimcore.plugin.datahub");
 
-    if (perspectiveCfg.inToolbar("datahub") === false) {
-        return
-    }
 
-    const user = pimcore.globalmanager.get("user");
-    if (user.admin || user.isAllowed("plugin_datahub_config")) {
+pimcore.plugin.datahub = Class.create({
+    getClassName: function () {
+        return "pimcore.plugin.datahub";
+    },
 
-        let navEl = Ext.get('pimcore_menu_search').insertSibling('<li id="pimcore_menu_datahub" data-menu-tooltip="'
-            + t('plugin_pimcore_datahub_toolbar') +
-            '" class="pimcore_menu_item pimcore_menu_needs_children"><img alt="datahub" src="/bundles/pimcoreadmin/img/flat-white-icons/mind_map.svg"></li>', 'before');
+    initialize: function () {
+        // if the new event exists, we use this
+        if (pimcore.events.preMenuBuild) {
+           // document.addEventListener(pimcore.events.preMenuBuild, this.preMenuBuild.bind(this));
+        } else {
 
-        navEl.on('mousedown', function () {
-            try {
-                pimcore.globalmanager.get("plugin_pimcore_datahub_config").activate();
-            } catch (e) {
-                pimcore.globalmanager.add("plugin_pimcore_datahub_config", new pimcore.plugin.datahub.config());
+        }
+        document.addEventListener(pimcore.events.pimcoreReady, this.pimcoreReady.bind(this));
+
+        document.addEventListener("pimcore.perspectiveEditor.permissions.structure.load", (e) => {
+            if (e.detail.context === 'toolbar') {
+                e.detail.structure['datahub'] = {};
             }
         });
 
-        pimcore.helpers.initMenuTooltips();
-    }
-});
+        document.addEventListener("pimcore.perspectiveEditor.permissions.load", (e) => {
+            const context = e.detail.context;
+            const menu = e.detail.menu;
+            const permissions = e.detail.permissions;
 
-document.addEventListener("pimcore.perspectiveEditor.permissions.structure.load", (e) => {
-    if (e.detail.context === 'toolbar') {
-        e.detail.structure['datahub'] = {};
-    }
-});
+            if (context === 'toolbar' && menu === 'datahub') {
+                if (permissions[context][menu] === undefined) {
+                    permissions[context][menu] = [];
+                }
+                if (permissions[context][menu].indexOf('hidden') === -1) {
+                    permissions[context][menu].push('hidden');
+                }
+            }
+        });
+    },
 
-document.addEventListener("pimcore.perspectiveEditor.permissions.load", (e) => {
-    const context = e.detail.context;
-    const menu = e.detail.menu;
-    const permissions = e.detail.permissions;
+    preMenuBuild: function (e) {
+        let menu = e.detail.menu;
 
-    if (context === 'toolbar' && menu === 'datahub') {
-        if (permissions[context][menu] === undefined) {
-            permissions[context][menu] = [];
+        menu.datahub = {
+            label: 'plugin_pimcore_datahub_toolbar',
+            iconCls: 'mind_map',
+            priority: 55,
+            shadow: false,
+            handler: this.openDataHub,
+            cls: "pimcore_navigation_flyout",
+            noSubmenus: true
+        };
+    },
+
+    openDataHub: function(e) {
+        try {
+            pimcore.globalmanager.get("plugin_pimcore_datahub_config").activate();
+        } catch (e) {
+            pimcore.globalmanager.add("plugin_pimcore_datahub_config", new pimcore.plugin.datahub.config());
         }
-        if (permissions[context][menu].indexOf('hidden') === -1) {
-            permissions[context][menu].push('hidden');
+    },
+
+    pimcoreReady: function(e) {
+        const perspectiveCfg = pimcore.globalmanager.get("perspective");
+
+        if (perspectiveCfg.inToolbar("datahub") === false) {
+            return
+        }
+
+        const user = pimcore.globalmanager.get("user");
+        if (user.admin || user.isAllowed("plugin_datahub_config")) {
+
+            let navEl = Ext.get('pimcore_menu_search').insertSibling('<li id="pimcore_menu_datahub" data-menu-tooltip="'
+                + t('plugin_pimcore_datahub_toolbar') +
+                '" class="pimcore_menu_item pimcore_menu_needs_children"><img alt="datahub" src="/bundles/pimcoreadmin/img/flat-white-icons/mind_map.svg"></li>', 'before');
+
+            navEl.on('mousedown', function () {
+                try {
+                    pimcore.globalmanager.get("plugin_pimcore_datahub_config").activate();
+                } catch (e) {
+                    pimcore.globalmanager.add("plugin_pimcore_datahub_config", new pimcore.plugin.datahub.config());
+                }
+            });
+
+            pimcore.helpers.initMenuTooltips();
         }
     }
 });
+
+var datahubPlugin = new pimcore.plugin.datahub();
