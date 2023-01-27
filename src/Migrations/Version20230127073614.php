299fc760855358a89eb8b08456557108fd30c37e
Corepex <Corepex@users.noreply.github.com>
2023-01-27 10:38:06 +0000

    Apply php-cs-fixer changes

--- src/Migrations/Version20230127073614.php
+++ src/Migrations/Version20230127073614.php
@@ -2,11 +2,24 @@
 
 declare(strict_types=1);
 
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
 namespace Pimcore\Bundle\DataHubBundle\Migrations;
 
-use Pimcore\Db;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Pimcore\Db;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -29,7 +42,7 @@ final class Version20230127073614 extends AbstractMigration
         $configurations = $db->fetchAllAssociative('SELECT * FROM `settings_store` WHERE scope = "pimcore_data_hub"');
         foreach ($configurations as $configuration) {
             $dataArray = json_decode($configuration['data'], true);
-            if(!array_key_exists('namespace', $dataArray)){
+            if (!array_key_exists('namespace', $dataArray)) {
                 $dataArray['namespace'] = $this->getNamespaceToType($dataArray['type']);
             }
             $configuration['data'] = json_encode($dataArray);
@@ -43,7 +56,7 @@ final class Version20230127073614 extends AbstractMigration
         $configurations = $db->fetchAllAssociative('SELECT * FROM `settings_store` WHERE scope = "pimcore_data_hub"');
         foreach ($configurations as $configuration) {
             $dataArray = json_decode($configuration['data'], true);
-            if(array_key_exists('namespace', $dataArray)){
+            if (array_key_exists('namespace', $dataArray)) {
                 unset($dataArray['namespace']);
             }
 
