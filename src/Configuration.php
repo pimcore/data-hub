fd675dc2601c11ae2abb519ef16c1ed04c77d1c1
Corepex <christian.feldkirchner@pimcore.com>
2023-01-27 14:05:29 +0100

    fixed api_key issue on import

--- src/Configuration.php
+++ src/Configuration.php
@@ -266,6 +266,10 @@ class Configuration extends AbstractModel
 
         $securityConfig = $this->getSecurityConfig();
         if (($this->configuration['general']['active'] ?? false) && isset($securityConfig['method']) && $securityConfig['method'] === self::SECURITYCONFIG_AUTH_APIKEY) {
+            if(!is_array($securityConfig['apikey'])){
+                $securityConfig['apikey'] = [$securityConfig['apikey']];
+            }
+
             foreach ($securityConfig['apikey'] as $apiKey) {
                 if (strlen($apiKey) < 16) {
                     throw new \Exception('API key ' . $apiKey . ' does not satisfy the minimum length of 16 characters');
