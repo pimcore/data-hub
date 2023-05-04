b0629883eba31b361012abf6b9c092db77cbefa6
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2023-05-04 09:45:27 +0200

    Update src/Migrations/PimcoreX/Version20230503165847.php
    Co-authored-by: Christian F. <christian.feldkirchner@pimcore.com>

--- src/Migrations/PimcoreX/Version20230503165847.php
+++ src/Migrations/PimcoreX/Version20230503165847.php
@@ -41,6 +41,7 @@ final class Version20230503165847 extends AbstractMigration
 
     private function replaceStringInFiles(string $search, string $replace): void
     {
+    if(is_dir(self::CONFIG_DIR)){
         $files = scandir(self::CONFIG_DIR, 0);
         for ($i = 2; $i < count($files); $i++) {
             $file = self::CONFIG_DIR . '/' . $files[$i];
@@ -48,5 +49,6 @@ final class Version20230503165847 extends AbstractMigration
             $fileContent = str_replace($search, $replace, $fileContent);
             file_put_contents($file, $fileContent);
         }
+    }
     }
 }
