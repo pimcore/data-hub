0317eb41d76884d9abb60248b7ecf60b6b35d290
Sebastian Blank <sebastian.bl@gmx.de>
2024-04-23 14:30:21 +0200

    [PhpCsFixer] Fix paths (#856)
    * [PhpCsFixer] Fix paths
    
    * [PhpCsFixer] Fix paths
    
    * [PhpCsFixer] Fix paths

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,20 +2,14 @@
 
 $finder = PhpCsFixer\Finder::create()
     ->in([
-        __DIR__ . '/bundles',
-        __DIR__ . '/config',
-        __DIR__ . '/lib',
-        __DIR__ . '/models',
-        __DIR__ . '/tests'
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
     ])
 
     ->exclude([
         __DIR__ . '/tests/_output',
-        __DIR__ . '/tests/Support/_generated',
+        __DIR__ . '/tests/_support/_generated',
     ])
-
-    // do not fix views
-    ->notName('*.html.php')
 ;
 
 // do not enable self_accessor as it breaks pimcore models relying on get_called_class()
