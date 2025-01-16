2a0ec88dc1f55cb1c0bca9962a72e32f74136c94
Al3x Zamponi <562324+alexz707@users.noreply.github.com>
2025-01-16 16:20:23 +0100

    cleanup of scripts (#932)

--- .github/ci/scripts/setup-environment.sh
+++ .github/ci/scripts/setup-environment.sh
@@ -13,11 +13,3 @@ chmod 755 bin/console
 cp -r .github/ci/files/kernel/. kernel
 cp -r .github/ci/files/public/. public
 
-if [ ${REQUIRE_ADMIN_BUNDLE} = true ]; then
-    #composer require -n --no-update pimcore/admin-ui-classic-bundle:^1.0
-    true
-else
-    ## Todo Remove when dropping support for Pimcore 10
-    rm config/packages/security.yaml
-    cp .github/ci/files/security.yaml config/packages
-fi
