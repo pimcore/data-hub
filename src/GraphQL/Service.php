ca412bca467032a7bb7df9c58061922155e975f5
mcop1 <89011527+mcop1@users.noreply.github.com>
2025-02-18 09:13:38 +0000

    Apply php-cs-fixer changes

--- src/GraphQL/Service.php
+++ src/GraphQL/Service.php
@@ -30,7 +30,6 @@ declare(strict_types=1);
 namespace Pimcore\Bundle\DataHubBundle\GraphQL;
 
 use Carbon\Carbon;
-use DateInvalidTimeZoneException;
 use GraphQL\Type\Definition\ResolveInfo;
 use Pimcore\Bundle\DataHubBundle\Configuration;
 use Pimcore\Bundle\DataHubBundle\GraphQL\Exception\ClientSafeException;
@@ -41,7 +40,6 @@ use Pimcore\Bundle\DataHubBundle\GraphQL\Query\Operator\Factory\OperatorFactoryI
 use Pimcore\Bundle\DataHubBundle\GraphQL\Query\Value\DefaultValue;
 use Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle;
 use Pimcore\Cache\RuntimeCache;
-use Pimcore\Config;
 use Pimcore\DataObject\GridColumnConfig\ConfigElementInterface;
 use Pimcore\Localization\LocaleServiceInterface;
 use Pimcore\Model\Asset;
