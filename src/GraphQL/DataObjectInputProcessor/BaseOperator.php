a85cb41157ccaa12f01c7156149fb8febf384d77
Marco Perberschlager <marco.perberschlager@pimcore.com>
2025-02-25 19:38:02 +0100

    Removed final keyword for some base classes

--- src/GraphQL/DataObjectInputProcessor/BaseOperator.php
+++ src/GraphQL/DataObjectInputProcessor/BaseOperator.php
@@ -19,7 +19,7 @@ use GraphQL\Type\Definition\ResolveInfo;
 use Pimcore\Model\DataObject\Concrete;
 use Pimcore\Model\DataObject\Fieldcollection\Data\AbstractData;
 
-final class BaseOperator extends Base
+class BaseOperator extends Base
 {
     /**
      * @param array $nodeDef
