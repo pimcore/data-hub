5288998be251e59fc4d5bc943ee3d900618728ed
Christian Fasching <fashxp@users.noreply.github.com>
2025-04-17 22:15:02 +0200

    Remove unused variable `$foo` in WebserviceController.

--- src/Controller/WebserviceController.php
+++ src/Controller/WebserviceController.php
@@ -99,7 +99,6 @@ final class WebserviceController extends FrontendController
     ) {
         $clientname = $request->attributes->getString('clientname');
         $variableValues = null;
-        $foo = '123';
 
         $configuration = Configuration::getByName($clientname);
         if (!$configuration || !$configuration->isActive()) {
