a75ea7f6fdd3212410cecb08c018ada31e53bf16
fashxp <8792145+fashxp@users.noreply.github.com>
2025-04-30 05:18:48 +0000

    Apply php-cs-fixer changes

--- src/GraphQL/Service.php
+++ src/GraphQL/Service.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
-* This source file is available under the terms of the
-* Pimcore Open Core License (POCL)
-* Full copyright and license information is available in
-* LICENSE.md which is distributed with this source code.
-*
-*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.com)
-*  @license    Pimcore Open Core License (POCL)
-*/
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
 declare(strict_types=1);
 
 /**
