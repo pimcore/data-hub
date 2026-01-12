b9f42f2233930606950ca9390249019568a2869f
Marco <89011527+mcop1@users.noreply.github.com>
2026-01-12 10:52:57 +0100

    Small adaptions for local test execution (#1014)

--- tests/bin/init-tests.sh
+++ tests/bin/init-tests.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-docker-compose down -v --remove-orphans
+docker compose down -v --remove-orphans
 
-docker-compose up -d
+docker compose up -d
 
-docker-compose exec php-fpm .github/ci/scripts/setup-environment.sh
+docker compose exec php-fpm .github/ci/scripts/setup-pimcore-environment.sh
 
-docker-compose exec php-fpm composer update
+docker compose exec php-fpm composer update
 #docker-compose exec php-fpm composer update --prefer-lowest --prefer-stable
 
-docker-compose exec php-fpm vendor/bin/codecept run -vv
+docker compose exec php-fpm vendor/bin/codecept run -vv
 
 printf "\n\n\n================== \n"
-printf "Run 'docker-compose exec php-fpm vendor/bin/codecept run -vv' to re-run the tests.\n"
-printf "Run 'docker-compose down -v --remove-orphans' to shutdown container and cleanup.\n\n"
+printf "Run 'docker compose exec php-fpm vendor/bin/codecept run -vv' to re-run the tests.\n"
+printf "Run 'docker compose down -v --remove-orphans' to shutdown container and cleanup.\n\n"
