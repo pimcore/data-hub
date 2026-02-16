704785c33df8bc4a466d6e07bf594eb32371015d
vin0401 <marvin.bertram@pimcore.com>
2026-02-16 15:08:51 +0100

    Update package information

--- src/Resources/public/studio/build/28adb3bd-4d42-4b51-8d19-16b5652ee8da/exposeRemote.js
+++ src/Resources/public/studio/build/28adb3bd-4d42-4b51-8d19-16b5652ee8da/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/6b60304a-58f7-4948-9ed0-1d88701cc7ee/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/28adb3bd-4d42-4b51-8d19-16b5652ee8da/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
