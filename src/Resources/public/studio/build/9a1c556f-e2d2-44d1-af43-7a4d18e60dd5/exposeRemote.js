4a447bfffb258b60e6c8f9aa5fd944b3c299d32e
umizoomi <4329209+umizoomi@users.noreply.github.com>
2026-02-19 10:42:19 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/9a1c556f-e2d2-44d1-af43-7a4d18e60dd5/exposeRemote.js
+++ src/Resources/public/studio/build/9a1c556f-e2d2-44d1-af43-7a4d18e60dd5/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/a65866f8-fc81-48d5-90f3-7502c5288b8c/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/9a1c556f-e2d2-44d1-af43-7a4d18e60dd5/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
