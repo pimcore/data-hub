46a54af56edc49b0aa781c0b395875829b2a6965
markus-moser <markus.moser@pimcore.com>
2026-02-24 18:20:04 +0100

    [Studio] Enter should trigger apply in operator modal (#1051)

---
+++ src/Resources/public/studio/build/72da68e5-2094-4eaa-ad10-59c33558affa/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/72da68e5-2094-4eaa-ad10-59c33558affa/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
