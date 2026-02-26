d7647c571b153786e4967e5a1967520c5fc0030b
JiaJia Ji <kingjia90@gmail.com>
2026-02-26 11:58:47 +0100

    [Task]: Update PHP and Symfony version requirements (#1050)
    * Update PHP and Symfony version requirements
    
    * Automatic frontend build
    
    * update composer
    
    * update composer
    
    * Automatic frontend build
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: kingjia90 <6014195+kingjia90@users.noreply.github.com>
    Co-authored-by: robertSt7 <robert.steinkellner@pimcore.com>
    Co-authored-by: robertSt7 <104770750+robertSt7@users.noreply.github.com>

---
+++ src/Resources/public/studio/build/6edee302-1625-465c-b227-0cd66ff4fdb0/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/6edee302-1625-465c-b227-0cd66ff4fdb0/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
