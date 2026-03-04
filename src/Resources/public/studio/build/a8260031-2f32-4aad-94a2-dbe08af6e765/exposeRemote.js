b92bfe0a3c95455c62a3154dde531206d4342504
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2026-03-04 09:58:04 +0100

    Update version constraint for studio-backend-bundle (#1055)
    * Update version constraint for studio-backend-bundle
    
    * Automatic frontend build
    
    * Update Codeception dependencies in composer.json
    
    * Automatic frontend build
    
    * update composer
    
    * Automatic frontend build
    
    * update composer
    
    * Automatic frontend build

--- src/Resources/public/studio/build/a8260031-2f32-4aad-94a2-dbe08af6e765/exposeRemote.js
+++ src/Resources/public/studio/build/a8260031-2f32-4aad-94a2-dbe08af6e765/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/2b5d117e-b444-4db3-ac26-409973fa39d8/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/a8260031-2f32-4aad-94a2-dbe08af6e765/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
