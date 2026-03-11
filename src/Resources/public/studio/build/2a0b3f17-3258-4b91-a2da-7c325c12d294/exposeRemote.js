5f17c23ce273be817bfcdb0fbc7d2dea6478cc7b
jcPimcore <259032526+jcPimcore@users.noreply.github.com>
2026-03-11 14:18:38 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/2a0b3f17-3258-4b91-a2da-7c325c12d294/exposeRemote.js
+++ src/Resources/public/studio/build/2a0b3f17-3258-4b91-a2da-7c325c12d294/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/2a5ffa9b-5312-4fe5-9e0f-57135556cf1f/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/2a0b3f17-3258-4b91-a2da-7c325c12d294/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
