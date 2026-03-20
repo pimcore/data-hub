1f4cbf183129c2d21ae292391650d140c760a7d1
bluvulture <7668379+bluvulture@users.noreply.github.com>
2026-03-20 09:35:04 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/bef45b31-a40c-4e60-8ed7-6a015649e96a/exposeRemote.js
+++ src/Resources/public/studio/build/bef45b31-a40c-4e60-8ed7-6a015649e96a/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/9f367eee-4305-4c6a-8eaf-66b2b902932f/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/bef45b31-a40c-4e60-8ed7-6a015649e96a/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
