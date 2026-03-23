d4c09eca8ba58ee0ac79e1d57c5dfb20edae485b
berfinyuksel <99557970+berfinyuksel@users.noreply.github.com>
2026-03-23 11:06:40 +0100

    chore: replace @reusable-workflows with @main (#1058)
    * chore: replace reusable-workflows branch references with main
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: berfin-yueksel <99557970+berfin-yueksel@users.noreply.github.com>

---
+++ src/Resources/public/studio/build/54aad01a-4577-4017-8cd8-5947eef0eb8d/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/54aad01a-4577-4017-8cd8-5947eef0eb8d/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
