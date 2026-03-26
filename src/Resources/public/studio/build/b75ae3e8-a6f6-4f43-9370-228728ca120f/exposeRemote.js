05fb5bbe599f27d8dc3c8e5931f11683f8a1af3f
fashxp <8792145+fashxp@users.noreply.github.com>
2026-03-26 10:21:02 +0000

    Automatic frontend build

---
+++ src/Resources/public/studio/build/b75ae3e8-a6f6-4f43-9370-228728ca120f/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/b75ae3e8-a6f6-4f43-9370-228728ca120f/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
