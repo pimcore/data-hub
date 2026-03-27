48f60c2b136cad0fd229ff47e42027e5807c7c9c
Marvin Bertram <marvin.bertram@pimcore.com>
2026-03-27 13:45:04 +0000

    feat: add MigrationModal component to SDK and expose via federation
    - Add reusable MigrationModal component to modules/config/components/migration-modal/
    - Export MigrationModal from SDK index so consumer bundles can import it via @pimcore/data-hub
    - Add 4 translation keys for data-hub.migration-modal.* to studio.en.yaml
    - Replace old build with new build (6eb98ca2)

--- src/Resources/public/studio/build/6eb98ca2-9574-4aba-8406-d567ec4536c9/exposeRemote.js
+++ src/Resources/public/studio/build/6eb98ca2-9574-4aba-8406-d567ec4536c9/exposeRemote.js
@@ -7,7 +7,11 @@
         window.alternativePluginExportPaths = {}
       }
 
+<<<<<<<< HEAD:src/Resources/public/studio/build/0787fb12-b036-4b44-a8e7-5d74ce0231a6/exposeRemote.js
       window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/0787fb12-b036-4b44-a8e7-5d74ce0231a6/static/js/remoteEntry.js"
+========
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/6eb98ca2-9574-4aba-8406-d567ec4536c9/static/js/remoteEntry.js"
+>>>>>>>> 261e552 (feat: add MigrationModal component to SDK and expose via federation):src/Resources/public/studio/build/6eb98ca2-9574-4aba-8406-d567ec4536c9/exposeRemote.js
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
