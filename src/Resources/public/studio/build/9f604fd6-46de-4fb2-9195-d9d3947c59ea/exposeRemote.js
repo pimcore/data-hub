fd56c27ed427c009d3c4b7dba61efd79742b6b47
Tine <42359615+xIrusux@users.noreply.github.com>
2026-03-31 12:21:21 +0200

    3207 - [Data Hub] - Translations (#1068)
    * Add translations for de, es, fr, it, no, sv
    
    * Automatic frontend build
    
    * Correct German translations in studio.de.yaml
    
    * Automatic frontend build
    
    * Remove unnecessary quotes from German translations
    
    * Automatic frontend build

---
+++ src/Resources/public/studio/build/9f604fd6-46de-4fb2-9195-d9d3947c59ea/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/9f604fd6-46de-4fb2-9195-d9d3947c59ea/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
