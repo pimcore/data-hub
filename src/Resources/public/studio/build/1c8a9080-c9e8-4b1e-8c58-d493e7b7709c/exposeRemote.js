f23ff809badcefda9872ba8a91a03d054d14dfe9
Tine <42359615+xIrusux@users.noreply.github.com>
2026-03-31 21:29:59 +0200

    3207 - [Data Hub] - Remove unnecessary quotes from translation YAML files (#1071)
    * chore(i18n): remove unnecessary quotes from translation YAML files
    
    * Automatic frontend build

--- src/Resources/public/studio/build/1c8a9080-c9e8-4b1e-8c58-d493e7b7709c/exposeRemote.js
+++ src/Resources/public/studio/build/1c8a9080-c9e8-4b1e-8c58-d493e7b7709c/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/a612a787-cafc-4bda-a11a-a6d46774b471/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/1c8a9080-c9e8-4b1e-8c58-d493e7b7709c/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
