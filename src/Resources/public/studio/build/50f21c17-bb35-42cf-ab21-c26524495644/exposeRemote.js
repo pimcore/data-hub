c5e1720d93f47004eb8b852a1581dc4fd6d89ff7
martineiber <11687066+martineiber@users.noreply.github.com>
2026-04-07 15:37:05 +0000

    Automatic frontend build

---
+++ src/Resources/public/studio/build/50f21c17-bb35-42cf-ab21-c26524495644/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/50f21c17-bb35-42cf-ab21-c26524495644/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
