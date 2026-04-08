939046e2cbb07fbe62a34be86cbd23d3a92b5ed4
Lukas Mzigot <30526586+lukmzig@users.noreply.github.com>
2026-04-08 11:04:10 +0200

    [2026.x] - event page does not work in editor (#1076)
    * fix: register controllers
    
    * Automatic frontend build

---
+++ src/Resources/public/studio/build/d1ce371e-b062-4f74-941b-a25b2154192f/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/d1ce371e-b062-4f74-941b-a25b2154192f/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
