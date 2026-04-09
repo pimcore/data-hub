9774d61b2f2c1ba8601c03a43723e423ddfe75e3
Marvin Bertram <marvin.bertram@pimcore.com>
2026-04-09 09:40:53 +0200

    Fix advanced column pipeline config lookup after title-as-key change (#1081)
    * Fix advanced column pipeline config lookup after title-as-key change
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: vin0401 <26813978+vin0401@users.noreply.github.com>

--- src/Resources/public/studio/build/3ddc30e4-25c5-4655-ace5-e112e89fd213/exposeRemote.js
+++ src/Resources/public/studio/build/3ddc30e4-25c5-4655-ace5-e112e89fd213/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/79124f9a-60c7-4e39-bea5-83138055a342/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/3ddc30e4-25c5-4655-ace5-e112e89fd213/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
