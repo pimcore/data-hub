a7c1618bfc23d8bfd0ed3313102fe35a4a887d1d
pimcore-deployments <71881008+pimcore-deployments@users.noreply.github.com>
2026-04-09 11:25:38 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/f933c918-a8bc-4f2c-93e5-14edc460dec9/exposeRemote.js
+++ src/Resources/public/studio/build/f933c918-a8bc-4f2c-93e5-14edc460dec9/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/bcd03c0d-0d91-46d8-97c7-050334d3c6df/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/f933c918-a8bc-4f2c-93e5-14edc460dec9/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
