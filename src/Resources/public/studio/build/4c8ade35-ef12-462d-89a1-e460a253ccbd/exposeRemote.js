5ccd1353bda22712c31b09f5eb00880ca4125eb5
kingjia90 <6014195+kingjia90@users.noreply.github.com>
2026-04-22 12:49:25 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/4c8ade35-ef12-462d-89a1-e460a253ccbd/exposeRemote.js
+++ src/Resources/public/studio/build/4c8ade35-ef12-462d-89a1-e460a253ccbd/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/0822da1a-af64-4d1c-85a7-803b44a63173/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/4c8ade35-ef12-462d-89a1-e460a253ccbd/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
