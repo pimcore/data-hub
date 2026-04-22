4fb76aac389abe901d67ffb43d47191d14ecb0c1
kingjia90 <6014195+kingjia90@users.noreply.github.com>
2026-04-22 12:31:54 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/699091e7-bdde-4ce2-8240-05d14cf712a7/exposeRemote.js
+++ src/Resources/public/studio/build/699091e7-bdde-4ce2-8240-05d14cf712a7/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/fae95ec2-4b81-4900-a85b-7135826117a5/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/699091e7-bdde-4ce2-8240-05d14cf712a7/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
