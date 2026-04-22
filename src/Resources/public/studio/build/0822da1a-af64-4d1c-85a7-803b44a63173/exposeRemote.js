136795c50edce3a59a80d9591c6966cd62c14ed6
JochenC <jochen.califice@pimcore.com>
2026-04-22 14:47:43 +0200

    [Task] Update PHPunit and Codeception (#1087)
    * [Task] Update PHPunit and Codeception
    
    * Automatic frontend build
    
    * Automatic frontend build
    
    * Update composer.json
    
    * Automatic frontend build
    
    * dummy change to re-run
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: jcPimcore <259032526+jcPimcore@users.noreply.github.com>
    Co-authored-by: kingjia90 <6014195+kingjia90@users.noreply.github.com>
    Co-authored-by: JiaJia Ji <kingjia90@gmail.com>

---
+++ src/Resources/public/studio/build/0822da1a-af64-4d1c-85a7-803b44a63173/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/0822da1a-af64-4d1c-85a7-803b44a63173/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
