17f99cf469eac63b9e1b48c221886f8660e019b4
JiaJia Ji <kingjia90@gmail.com>
2026-04-22 15:33:25 +0200

    [Task] Update PHPunit and Codeception (#1090)
    * replace setMethods with onlyMethods
    
    * Automatic frontend build
    
    * sync workflow file
    
    * Automatic frontend build
    
    * revert concurrency:
    
    * replacement for deprecated getMockForTrait
    
    * Automatic frontend build
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: kingjia90 <6014195+kingjia90@users.noreply.github.com>
    (cherry picked from commit e67eaf634e226fc41d3b01167ca5d895a07e997f)

---
+++ src/Resources/public/studio/build/c8163389-0e9e-4381-a265-b3aa44afb6e2/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/c8163389-0e9e-4381-a265-b3aa44afb6e2/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
