80b6d928b20882936902da410cac28841e9e4509
vin0401 <26813978+vin0401@users.noreply.github.com>
2026-04-27 08:51:24 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/ab2e65dc-9820-4962-94cb-1744af8f534f/exposeRemote.js
+++ src/Resources/public/studio/build/ab2e65dc-9820-4962-94cb-1744af8f534f/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/f50fb26a-2424-482d-b398-70310601022f/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/ab2e65dc-9820-4962-94cb-1744af8f534f/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
