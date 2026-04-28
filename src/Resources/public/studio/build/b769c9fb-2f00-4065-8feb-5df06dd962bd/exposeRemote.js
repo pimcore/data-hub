3feb96d98a145bdd960b9d795bf469cd7b465c15
markus-moser <markus.moser@pimcore.com>
2026-04-28 17:00:27 +0200

    [Studio] Prepare space to add queue item count to config toolbar (#1096)

--- src/Resources/public/studio/build/b769c9fb-2f00-4065-8feb-5df06dd962bd/exposeRemote.js
+++ src/Resources/public/studio/build/b769c9fb-2f00-4065-8feb-5df06dd962bd/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/72887837-d3cf-44c1-8a5e-ea2023e60142/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/b769c9fb-2f00-4065-8feb-5df06dd962bd/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
