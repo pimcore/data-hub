595a025d8306ddc4212de3c51c59d3ec601590ac
Marvin Bertram <marvin.bertram@pimcore.com>
2026-04-29 09:35:18 +0200

    Improve the behavior of localisation inside of schema modals (#1097)
    * Improve the behavior of localisation inside of schema modals
    
    * Fix unsafe member access lint error on contentLanguages
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: vin0401 <26813978+vin0401@users.noreply.github.com>

---
+++ src/Resources/public/studio/build/24bb9b11-103f-4edd-b488-01683a23d540/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/24bb9b11-103f-4edd-b488-01683a23d540/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
