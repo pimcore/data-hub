5e9c5edd87c935bca3465d70f809d94b2252301b
berfinyuksel <99557970+berfinyuksel@users.noreply.github.com>
2026-05-15 10:21:42 +0200

    Delete backup workflow files (#1098)
    * Delete backup workflow files
    
    * Automatic frontend build

---
+++ src/Resources/public/studio/build/fe68136f-356a-4070-8bf5-d185fbe01f75/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/fe68136f-356a-4070-8bf5-d185fbe01f75/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
