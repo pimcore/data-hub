c34dcf93a84c903e93b1cd1348362bcf17eb18e9
blankse <998558+blankse@users.noreply.github.com>
2026-06-09 12:47:45 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/514ba5dc-5828-4f5e-93dd-592cd74db961/exposeRemote.js
+++ src/Resources/public/studio/build/514ba5dc-5828-4f5e-93dd-592cd74db961/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/785fd5b2-5311-4070-9fc4-81949c904ac6/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/514ba5dc-5828-4f5e-93dd-592cd74db961/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
