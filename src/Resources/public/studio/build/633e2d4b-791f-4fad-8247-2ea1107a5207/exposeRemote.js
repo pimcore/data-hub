daf216dfec0f4a960e4b50b640bab7ffe1a8277f
astapc <210384374+astapc@users.noreply.github.com>
2026-06-15 13:47:17 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/633e2d4b-791f-4fad-8247-2ea1107a5207/exposeRemote.js
+++ src/Resources/public/studio/build/633e2d4b-791f-4fad-8247-2ea1107a5207/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/ce357842-7dfd-46ae-9fd3-897e4c6c6ffe/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/633e2d4b-791f-4fad-8247-2ea1107a5207/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
