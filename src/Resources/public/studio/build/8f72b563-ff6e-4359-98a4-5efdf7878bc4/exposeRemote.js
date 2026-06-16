65187efc60a761a28d4fdd949120c7118d26fe1c
markus-moser <4639428+markus-moser@users.noreply.github.com>
2026-06-16 16:51:36 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/8f72b563-ff6e-4359-98a4-5efdf7878bc4/exposeRemote.js
+++ src/Resources/public/studio/build/8f72b563-ff6e-4359-98a4-5efdf7878bc4/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/6ce54011-db73-4847-b2de-3a0815583179/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/8f72b563-ff6e-4359-98a4-5efdf7878bc4/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
