e4f31e6dc729f8eedda6fcf550d2c4002461e6ad
markus-moser <markus.moser@pimcore.com>
2026-06-18 15:46:00 +0200

    [Studio] Enforce data hub adapter permissions in config UI (#1107)

--- src/Controller/Studio/Config/CollectionController.php
+++ src/Controller/Studio/Config/CollectionController.php
@@ -18,6 +18,7 @@ use Pimcore\Bundle\DataHubBundle\OpenApi\Config\Prefix;
 use Pimcore\Bundle\DataHubBundle\OpenApi\Config\Tags;
 use Pimcore\Bundle\DataHubBundle\Schema\Configuration;
 use Pimcore\Bundle\DataHubBundle\Service\Studio\ConfigurationServiceInterface;
+use Pimcore\Bundle\DataHubBundle\Utils\Constants\PermissionConstants;
 use Pimcore\Bundle\StudioBackendBundle\Controller\AbstractApiController;
 use Pimcore\Bundle\StudioBackendBundle\Exception\Api\InvalidArgumentException;
 use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Property\GenericCollection;
@@ -28,6 +29,7 @@ use Pimcore\Bundle\StudioBackendBundle\Util\Constant\HttpResponseCodes;
 use Pimcore\Bundle\StudioBackendBundle\Util\Trait\PaginatedResponseTrait;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\SerializerInterface;
 
 /**
@@ -65,6 +67,7 @@ final class CollectionController extends AbstractApiController
         description: 'bundle_copilot_actions_success_response',
         content: new CollectionJson(new GenericCollection(Configuration::class)),
     )]
+    #[IsGranted(PermissionConstants::PLUGIN_DATA_HUB_CONFIG)]
     #[DefaultResponses([
         HttpResponseCodes::UNAUTHORIZED,
         HttpResponseCodes::NOT_FOUND,
--- src/Resources/public/studio/build/485dc6d1-5bd9-43b8-85a9-c2de74157236/exposeRemote.js
+++ src/Resources/public/studio/build/485dc6d1-5bd9-43b8-85a9-c2de74157236/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/08329004-8c3e-44d0-9d46-c042337d2a3d/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/485dc6d1-5bd9-43b8-85a9-c2de74157236/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
