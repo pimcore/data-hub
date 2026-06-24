b834d80d2eb86e8c4e96160bfa0325a747709055
Marvin Bertram <marvin.bertram@pimcore.com>
2026-06-24 07:53:31 +0200

    Add new column picker for schema definitions (#1110)
    * Add new column picker for schema definitions
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: vin0401 <26813978+vin0401@users.noreply.github.com>

---
+++ src/Resources/public/studio/build/754e33b3-a297-42fe-81b7-e3573d16308b/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/754e33b3-a297-42fe-81b7-e3573d16308b/static/js/remoteEntry.js"
+
+      window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
+    
