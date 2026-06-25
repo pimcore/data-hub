27683bd958d605c73584ab189c40fcadcae57499
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2026-06-25 11:57:37 +0200

    Replace old route with new studio route (#1111)
    * Replace old route with new studio route
    
    Signed-off-by: Matthias Hamacher <m.hamacher@basecom.de>
    
    * Automatic frontend build
    
    * Fix: csp for playground
    
    * Automatic frontend build
    
    ---------
    
    Signed-off-by: Matthias Hamacher <m.hamacher@basecom.de>
    Co-authored-by: Matthias Hamacher <m.hamacher@basecom.de>

--- src/Resources/public/studio/build/c25c0f68-3689-4beb-b331-d81079014992/exposeRemote.js
+++ src/Resources/public/studio/build/c25c0f68-3689-4beb-b331-d81079014992/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/72d10bd3-3693-44fe-81f0-eecd45be45bd/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/c25c0f68-3689-4beb-b331-d81079014992/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
