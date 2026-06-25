1b927ba57235883197fbea785f33e4a37a3cbd4c
matthiashamacher <32356549+matthiashamacher@users.noreply.github.com>
2026-06-25 09:06:41 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/d961c786-b270-4bed-b2b4-573afa3369e9/exposeRemote.js
+++ src/Resources/public/studio/build/d961c786-b270-4bed-b2b4-573afa3369e9/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/a52fed85-37e6-4d3d-b9f9-1a9dcbc8a8ff/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/d961c786-b270-4bed-b2b4-573afa3369e9/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
