6b781891cba97773603420b7b1ed7329f9628dc5
markus-moser <markus.moser@pimcore.com>
2026-06-29 14:39:49 +0200

    Prepare 2026.1.3 Release (#1114)

--- src/Resources/public/studio/build/af99363b-eaf1-43e5-828b-d0f8b3ecea01/exposeRemote.js
+++ src/Resources/public/studio/build/af99363b-eaf1-43e5-828b-d0f8b3ecea01/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/377dc827-8da0-4266-b8ff-2a67339262c7/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/af99363b-eaf1-43e5-828b-d0f8b3ecea01/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
