c7d3fc574692ee47e8745873f36ef903a18a7808
mcop1 <89011527+mcop1@users.noreply.github.com>
2026-07-07 12:18:48 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/ce0d0829-297e-460f-a72f-8ec48a559cf2/exposeRemote.js
+++ src/Resources/public/studio/build/ce0d0829-297e-460f-a72f-8ec48a559cf2/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/76efb54a-a043-45fb-a157-d0381e51d590/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/ce0d0829-297e-460f-a72f-8ec48a559cf2/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
