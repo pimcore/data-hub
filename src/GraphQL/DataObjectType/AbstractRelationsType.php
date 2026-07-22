e78450ebc52235a79cade0ca04dbb7964a99da4d
Sebastian Blank <blank@data-factory.net>
2026-07-22 10:34:20 +0200

    Fix folder relations in AbstractRelationsType::getTypes()
    getTypes() builds the union member types eagerly (via Schema::getTypeMap()).
    For a relation field that allows object folders, the 'folder' pseudo-class
    reached ClassTypeDefinitions::get('folder') and threw
    "type definition folder not found", failing every query against the schema.
    
    Route 'folder' to the _object_folder type, mirroring the resolveType() fix.

--- src/GraphQL/DataObjectType/AbstractRelationsType.php
+++ src/GraphQL/DataObjectType/AbstractRelationsType.php
@@ -102,6 +102,11 @@ abstract class AbstractRelationsType extends UnionType implements ContainerAware
                     if (is_array($className)) {
                         $className = $className['classes'];
                     }
+                    if ($className === 'folder') {
+                        $types[] = $this->getGraphQlService()->getDataObjectTypeDefinition('_object_folder');
+
+                        continue;
+                    }
                     $types[] = ClassTypeDefinitions::get($className);
                 }
             }
