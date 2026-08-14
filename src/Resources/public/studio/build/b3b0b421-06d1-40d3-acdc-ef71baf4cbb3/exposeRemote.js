6d6ce695af93807ce8565f76395307769342d317
berfinyuksel <99557970+berfinyuksel@users.noreply.github.com>
2026-08-14 13:48:14 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/b3b0b421-06d1-40d3-acdc-ef71baf4cbb3/exposeRemote.js
+++ src/Resources/public/studio/build/b3b0b421-06d1-40d3-acdc-ef71baf4cbb3/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/c4ff929d-1548-446c-819b-7d6615e88815/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/b3b0b421-06d1-40d3-acdc-ef71baf4cbb3/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
