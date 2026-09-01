88354c2f8268d798a644355a1ba3fff4d79b5893
ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>
2026-09-01 16:11:57 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/0529b08a-d5fe-43c8-9027-74fefbaae84d/exposeRemote.js
+++ src/Resources/public/studio/build/0529b08a-d5fe-43c8-9027-74fefbaae84d/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/acf21d12-a694-47bb-8257-f33d4a4e1d68/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/0529b08a-d5fe-43c8-9027-74fefbaae84d/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
