81e4bd30f35c64d38dc18ed22a8787296df0e0ba
sholzer <10495949+sholzer@users.noreply.github.com>
2026-09-18 11:13:14 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/f3fa91f3-a6ea-4c00-bdb3-d82c21a097e2/exposeRemote.js
+++ src/Resources/public/studio/build/f3fa91f3-a6ea-4c00-bdb3-d82c21a097e2/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/17a0f88f-8694-40ec-bb2b-cf61329d8b8b/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_datahub_bundle = "/bundles/pimcoredatahub/studio/build/f3fa91f3-a6ea-4c00-bdb3-d82c21a097e2/static/js/remoteEntry.js"
 
       window.alternativePluginExportPaths.pimcore_datahub_bundle = "/plugins"
     
